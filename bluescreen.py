4620ba5da86f5429b33fe855b74c53789d2657c6
Yushiway <81389581+Yushiway@users.noreply.github.com>
2024-01-30 20:15:43 -0600

    Update bluescreen.py

--- bluescreen.py
+++ bluescreen.py
@@ -10,15 +10,10 @@ class BlueScreen(pixelstrip.Animation):
 
     def reset(self, matrix):
         self.timeout = 0.0
-    
-    def fill(self, r, g, b):
-        for i in range(matrix.width):
-            for o in range(matrix.height):
-                matrix[i, o] = (r, g, b, 0)
 
     def draw(self, matrix, delta_time):
         if self.is_timed_out():
-            self.fill(0, 0, 255)
+            matrix.fill((0, 0, 255))
             matrix.show()
 
 matrix = pixelstrip.PixelStrip(board.D12, width=8, height=8, bpp=4, pixel_order=pixelstrip.GRB, options={pixelstrip.MATRIX_TOP, pixelstrip.MATRIX_LEFT, pixelstrip.MATRIX_ZIGZAG, pixelstrip.MATRIX_COLUMN_MAJOR})
