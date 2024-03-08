b5af0a926a68b22d47ee06de978dd349a6539e4a
Cal Schwick <35741152+CalSch@users.noreply.github.com>
2024-03-07 22:03:28 -0600

    Added a customizable width and height to conways game of life

--- conways_game_of_life.py
+++ conways_game_of_life.py
@@ -15,11 +15,17 @@
                 ||     ||
 """
 import pixelstrip
-import board 
+import board
 from colors import *
 import math
 import random
 
+# The width and height can be customized!
+width=8
+height=8
+pixels=width*height
+
+# Uncomment this to have a preset screen
 # screen=[
 #     0,1,0,0,0,0,0,0,
 #     0,0,1,0,0,0,0,0,
@@ -32,22 +38,25 @@ import random
 # ]
 screen=[]
 new_screen=[]
-for i in range(64):
+for i in range(pixels):
     screen.append(random.randint(0,1)==0)
 new_screen=screen.copy()
+
 neighbor_indices=[
-    -9,-8,-7, -1, 1, 7, 8, 9
+    -width-1, -width, -width+1,
+          -1,                1,
+     width-1,  width,  width+1
 ]
 scalar=float
 def hsv_to_rgb( h:scalar, s:scalar, v:scalar, a:scalar ) -> tuple:
     if s:
         if h == 1.0: h = 0.0
         i = int(h*6.0); f = h*6.0 - i
-        
+
         w = v * (1.0 - s)
         q = v * (1.0 - s * f)
         t = v * (1.0 - s * (1.0 - f))
-        
+
         if i==0: return (v, t, w, a)
         if i==1: return (q, v, w, a)
         if i==2: return (w, v, t, a)
@@ -62,7 +71,7 @@ class Coder(pixelstrip.Animation):
         for i in range(length):
                 if y-i >= 0 and y-i <= strip.n:
                     if i == 0:
-                        strip[y-i] = (0, 0, 0, 0)                   
+                        strip[y-i] = (0, 0, 0, 0)
                     if i < length/2 and not i == 0:
                         strip[y-i] = (0, int(abs(0.5*5*math.cos(math.pi*i/(length*0.1)))), 0, 0)
                     if i == length/2:
@@ -79,11 +88,11 @@ class Coder(pixelstrip.Animation):
     def draw(self, strip, delta_time):
         global screen,new_screen,neighbor_indices
         if self.is_timed_out():
-            self.timeout = 0.1
-            for i in range(64):
+            self.timeout = 0.3
+            for i in range(pixels):
                 n=0
                 for j in neighbor_indices:
-                    n+=screen[(i+j)%64]
+                    n+=screen[(i+j)%pixels]
                 if screen[i]:
                     if n<=1 or n>=4:
                         new_screen[i]=False
@@ -96,23 +105,26 @@ class Coder(pixelstrip.Animation):
                 #     new_screen[i]=False
                 #     new_screen[min(i+1,63)]=True
                 #     pass
-            if random.randint(0,20)==0:
-                for i in range(15):
-                    new_screen[random.randint(0,63)]=random.randint(0,1)
+            # if random.randint(0,20)==0:
+            #     for i in range(45):
+            #         new_screen[random.randint(0,pixels-1)]=random.randint(0,1)
+            if sum(screen)<=15:
+                for i in range(pixels):
+                    new_screen[i]=(random.randint(0,1)==0)
             screen=new_screen.copy()
             # strip[self.t%64]=(self.t,0,self.t/100,0)
-            for i in range(64):
-                y=int(i/8)
-                x=i%8
+            for i in range(pixels):
+                y=int(i/width)
+                x=i%width
                 if y%2==0:
-                    x=7-x
-                strip[x,y]=hsv_to_rgb((self.t/100)%1,0.75,10,0) if screen[i] else BLACK
+                    x=(width-1)-x
+                strip[x,y]=hsv_to_rgb((self.t/100)%1,0.8,25,0) if screen[i] else BLACK
             self.t+=1
             strip.show()
 
 
 if __name__ == "__main__":
-    strip = pixelstrip.PixelStrip(board.D12, width=8, height=8, bpp=4, pixel_order=pixelstrip.GRB)
+    strip = pixelstrip.PixelStrip(board.D15, width=width, height=height, bpp=4, pixel_order=pixelstrip.GRB) # the `board.D15` didn't work on one of the RPis that I used, if it doesn't work replace it with `board.GP15`
     strip.timeout = 0.7
 
     strip.animation = Coder()
