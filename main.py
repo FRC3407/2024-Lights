90b8eeb486146bc8c772f83898b760573827660f
Keith Rieck <KeithRieck@gmail.com>
2024-03-08 21:04:51 -0600

    Updated pin numbers for pixels and I2C. Added .gitignore.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import digitalio
 import board
-from circle_spinner import 
+from circle_spinner import CircleSpinner
 from i2ctarget import I2CTarget
 from pixelstrip import PixelStrip, current_time
 from animation_pulse import PulseAnimation
+from colors import *
 
 I2C_ADDRESS = 0x41
 BRIGHTNESS = 0.5
@@ -17,16 +18,16 @@ animation = [
 strip = [
     PixelStrip(board.NEOPIXEL0, 8, bpp=4, pixel_order="RGB", brightness=BRIGHTNESS),
     PixelStrip(board.NEOPIXEL1, 8, bpp=4, pixel_order="RGB", brightness=BRIGHTNESS),
+    PixelStrip(board.NEOPIXEL2, 8, bpp=4, pixel_order="RGB", brightness=BRIGHTNESS),
     PixelStrip(board.NEOPIXEL3, 8, bpp=4, pixel_order="RGB", brightness=BRIGHTNESS),
-    PixelStrip(board.NEOPIXEL4, 8, bpp=4, pixel_order="RGB", brightness=BRIGHTNESS),
-    PixelStrip(board.NEOPIXEL5, 8, bpp=4, pixel_order="RGB", brightness=BRIGHTNESS)
+    PixelStrip(board.NEOPIXEL4, 8, bpp=4, pixel_order="RGB", brightness=BRIGHTNESS)
 ]
 
 # The built-in LED will turn on for half a second after every message
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
-i2c = I2CTarget(scl=board.GP7, sda=board.GP6, addresses=[I2C_ADDRESS])
+i2c = I2CTarget(scl=board.SCL, sda=board.SDA, addresses=[I2C_ADDRESS])
 
 # Receive one byte through I2C, if available
 def receive_message():
@@ -57,4 +58,5 @@ def main():
             last_msg_time = current_time()
         led.value = (current_time() < last_msg_time + 0.5)
 
-main()
+if __name__ == "__main__":
+    main()
