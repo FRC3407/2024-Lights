fdb745959b35f1d788817d9ae11982e47bd9a2fc
Keith <KeithRieck@gmail.com>
2024-04-01 19:35:57 -0500

    Added offset value to PixelStrips, to work with the new green board

--- main.py
+++ main.py
@@ -53,11 +53,11 @@ animation = [
 
 # List of PixelStrips
 strip = [
-    PixelStrip(board.NEOPIXEL0, 144, bpp=4, pixel_order="GRB", brightness=BRIGHTNESS),
-    PixelStrip(board.NEOPIXEL1, width=32, height=8, bpp=4, pixel_order="GRB", brightness=BRIGHTNESS, options={pixelstrip.MATRIX_TOP, pixelstrip.MATRIX_LEFT, pixelstrip.MATRIX_COLUMN_MAJOR, pixelstrip.MATRIX_ZIGZAG}),
-    PixelStrip(board.NEOPIXEL2, width=8, height=8, bpp=4, pixel_order="GRB", brightness=BRIGHTNESS),
-    PixelStrip(board.NEOPIXEL3, 24, bpp=4, pixel_order="GRB", brightness=BRIGHTNESS),
-    PixelStrip(board.NEOPIXEL4, 24, bpp=4, pixel_order="GRB", brightness=BRIGHTNESS)
+    PixelStrip(board.NEOPIXEL0, 144, bpp=4, offset=1, pixel_order="GRB", brightness=BRIGHTNESS),
+    PixelStrip(board.NEOPIXEL1, width=32, offset=1, height=8, bpp=4, pixel_order="GRB", brightness=BRIGHTNESS, options={pixelstrip.MATRIX_TOP, pixelstrip.MATRIX_LEFT, pixelstrip.MATRIX_COLUMN_MAJOR, pixelstrip.MATRIX_ZIGZAG}),
+    PixelStrip(board.NEOPIXEL2, width=8, offset=1, height=8, bpp=4, pixel_order="GRB", brightness=BRIGHTNESS),
+    PixelStrip(board.NEOPIXEL3, 24, bpp=4, offset=1, pixel_order="GRB", brightness=BRIGHTNESS),
+    PixelStrip(board.NEOPIXEL4, 24, bpp=4, offset=1, pixel_order="GRB", brightness=BRIGHTNESS)
 ]
 
 # The built-in LED will turn on for half a second after every message
